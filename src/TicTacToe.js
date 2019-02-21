c3c141e5f2803220197a9ceaaf8b3cbc34569421
Sophia Dizon <sophia.dizon@mail.utoronto.ca>
2019-02-21 13:57:15 -0500

    Added a game home page for TicTacToe.

---
+++ src/TicTacToe.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class TicTacToe extends Component {
+  render() {
+
+    return (
+      <div>
+      Tic Tac Toe !
+      </div>
+    );
+  }
+}
+
+export default TicTacToe;
