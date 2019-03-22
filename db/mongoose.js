446b9cf7552eebdc73cc6b033495fa23296b1286
Anusha Fazal <anusha.fazal@mail.utoronto.ca>
2019-03-22 13:07:29 -0400

    Modify db

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 // connect to our database
-mongoose.connect('mongodb+srv://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PW + '@arcadebattle-cluster-bivpz.mongodb.net/arcade_battle?retryWrites=true', { useNewUrlParser: true, dbName: 'arcade_battle'});
+mongoose.connect('mongodb+srv://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PW + '@arcadebattle-cluster-bivpz.mongodb.net/arcade_battle?retryWrites=true', { useNewUrlParser: true});
 
 
 module.exports = { mongoose }
