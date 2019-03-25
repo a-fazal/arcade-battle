88bc1879ee42ce42c31dd6f36ae1ca90aafaf24a
Sophia Dizon <sophia.dizon@gmail.com>
2019-03-25 14:46:07 -0400

    Completed HTML layout for in game checkers.

--- src/CheckersInGame.js
+++ src/CheckersInGame.js
@@ -5,25 +5,15 @@ var delay = 3;
 class CheckersInGame extends Component {
 constructor(props) {
    super(props)
-   this.onItemClick = this.onItemClick.bind(this);
    this.state = {
-     turn: 'o',
-     'top-left-ttt': 'empty',
-     'top-center-ttt': 'empty',
-     'top-right-ttt': 'empty',
-     'middle-left-ttt': 'empty',
-     'middle-center-ttt': 'empty',
-     'middle-right-ttt': 'empty',
-     'bottom-left-ttt': 'empty',
-     'bottom-center-ttt': 'empty',
-     'bottom-right-ttt': 'empty',
+     turn: 'black',
      me: null,
      them: null,
-     end: false
+     end: false,
+     board: []
    }
+   this.onItemClick = this.onItemClick.bind(this);
    this.checkForOpponent = this.checkForOpponent.bind(this);
-   this.checkForEndGame = this.checkForEndGame.bind(this);
-   this.checkForWin = this.checkForWin.bind(this);
    this.gameOverMessage = this.gameOverMessage.bind(this);
 }
 
@@ -46,13 +36,13 @@ checkForOpponent() {
         username: "Sophia",
         winstreak: 21,
         id: 6,
-        symbol: 'x'
+        colour: "black"
       },
       them: {
         username: "Alec",
         winstreak: 14,
         id: 14,
-        symbol: 'o'
+        colour: "red"
       }
     };
   } else {
@@ -67,203 +57,255 @@ checkForOpponent() {
   }
 }
 
+/* TO BE COMPLETED */
 onItemClick(e) {
     e.preventDefault();
-    if (!this.state.end && this.state[e.currentTarget.id] === 'empty'){
-      if (this.state.turn === 'o') {
-        const turn = this.state.turn;
-        this.setState({[e.currentTarget.id]: 'o'}, () => {
-            this.checkForEndGame(turn);
-        });
-        this.setState({turn:'x'});
-      } else {
-        const turn = this.state.turn;
-        this.setState({[e.currentTarget.id]: 'x'}, () => {
-            this.checkForEndGame(turn);
-        });
-        this.setState({turn: 'o'});
-      }
-    }
-  }
+    return;
+ }
 
+/* TO BE COMPLETED */
 gameOverMessage(turn) {
-  if (turn === this.state.me.symbol) {
-    alert(this.state.me.username + " wins!");
-    const me = this.state.me;
-    me.winstreak = this.state.me.winstreak + 1;
-    console.log(me);
-    this.setState({me: me});
-  } else if (turn === this.state.them.symbol) {
-    alert(this.state.them.username + " wins!");
-    const them = this.state.them;
-    them.winstreak = this.state.them.winstreak + 1;
-    console.log(them);
-    this.setState({them: them});
-  }
-}
-
-  checkForWin(spotsToCheck, turn, diagonal) {
-  if (!diagonal) {
-    const spots = Object.keys(this.state);
-    const spotsResult = spots.filter(function (spot) { return spot.includes(spotsToCheck); });
-    let i;
-    for (i = 0; i < 3; i++){
-      if (this.state[spotsResult[i]] !== turn) {
-        return false;
-      }
-    }
-  } else {
-    if ((this.state['top-left-ttt'] !== turn || this.state['middle-center-ttt'] !== turn || this.state['bottom-right-ttt'] !== turn)
-      && (this.state['bottom-left-ttt'] !== turn || this.state['middle-center-ttt'] !== turn || this.state['top-right-ttt'] !== turn)) {
-      return false;
-    }
-  }
-  return true;
-  }
-
-checkForEndGame(turn){
-  // Check horizontal
-  if (this.checkForWin('top', turn, false) || this.checkForWin('middle', turn, false) || this.checkForWin('bottom', turn, false)) {
-    this.setState({end: true});
-    this.gameOverMessage(turn);
-    return;
-  }
-
-  // Check vertical
-  if (this.checkForWin('left', turn, false) || this.checkForWin('right', turn, false) || this.checkForWin('center', turn, false)) {
-    this.setState({end: true});
-    this.gameOverMessage(turn);
-    return;
-  }
-
-  // Check diagonals
-  if (this.checkForWin('', turn, true)) {
-    this.setState({end: true});
-    this.gameOverMessage(turn);
-    return;
-  }
-
-  // Check if no more possible moves
-  const spots = Object.keys(this.state);
-  let i;
-  for (i = 0; i < 9; i++){
-    if (this.state[spots[i]] === 'empty') {
-      this.setState({end: false});
-      return;
-    }
-  }
-  alert("Game over, it's a tie!");
-  this.setState({end: true});
-}
+	return;
+ }
 
   render() {
     if (this.state.them) {
       return (
-        <div id="inGame">
-          <div id="inGameHeader">
-            <div id="myStats" className="player-stats">
-              <div className="player-identity">
-                <div className="player-name text-center">{this.state.me.username}</div>
-                <div className="player-avatar"></div>
-              </div>
-            </div>
+        <div id=inGame>
+        	<div class="inGameHeader">
+        		<div id="myStats" className="player-stats">
+        			<div className="player-identity">
+                		<div className="player-name text-center">{this.state.me.username}</div>
+                		<div className="player-avatar"></div>
+              		</div>
+        		</div>
 
-            <div id="winstreaks">
-              <div className="winstreak-label text-center">WINSTREAKS</div>
-              <div className="winstreaks-amount text-center">
-                {this.state.me.winstreak}:{this.state.them.winstreak}
-              </div>
-            </div>
+        		<div id="winstreaks">
+              		<div className="winstreak-label text-center">WINSTREAKS</div>
+              		<div className="winstreaks-amount text-center">
+                	{this.state.me.winstreak}:{this.state.them.winstreak}
+              		</div>
+            	</div>
 
-            <div id="theirStats" className="player-stats">
-              <div className="player-identity">
-                <div className="player-name text-center">{this.state.them.username}</div>
-                <div className="player-avatar"></div>
-              </div>
-            </div>
-          </div>
-          <table id="checkersboard">
-          <tbody>
-            <tr>
-              <td id="top-left-ttt" onClick={this.onItemClick}>
-              {this.state['top-left-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['top-left-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-              <td id="top-center-ttt" onClick={this.onItemClick}>
-              {this.state['top-center-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['top-center-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-              <td id="top-right-ttt" onClick={this.onItemClick}>
-              {this.state['top-right-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['top-right-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-            </tr>
-            <tr>
-              <td id="middle-left-ttt" onClick={this.onItemClick}>
-              {this.state['middle-left-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['middle-left-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-              <td id="middle-center-ttt" onClick={this.onItemClick}>
-              {this.state['middle-center-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['middle-center-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-              <td id="middle-right-ttt" onClick={this.onItemClick}>
-              {this.state['middle-right-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['middle-right-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-            </tr>
-            <tr>
-              <td id="bottom-left-ttt" onClick={this.onItemClick}>
-              {this.state['bottom-left-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['bottom-left-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-              <td id="bottom-center-ttt" onClick={this.onItemClick}>
-              {this.state['bottom-center-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['bottom-center-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-              <td id="bottom-right-ttt" onClick={this.onItemClick}>
-              {this.state['bottom-right-ttt'] === 'o' &&
-                <i className="far fa-circle ttt-circle-all"></i>
-              }
-              {this.state['bottom-right-ttt'] === 'x' &&
-                <i className="fas fa-times ttt-x-all"></i>
-              }
-              </td>
-            </tr>
-            </tbody>
-          </table>
+            	<div id="theirStats" className="player-stats">
+              		<div className="player-identity">
+                		<div className="player-name text-center">{this.state.them.username}</div>
+                		<div className="player-avatar"></div>
+              		</div>
+            	</div>
+        	</div>
+
+        	<table id="checkersBoard">
+        		<tbody>
+        			<tr>
+        				<td id="row1col1" className="redboard">
+        				</td>
+
+        				<td id="row1col2" className="blackboard">
+        				</td>
+
+        				<td id="row1col3" className="redboard">
+        				</td>
+
+        				<td id="row1col4" className="blackboard">
+        				</td>
+
+        				<td id="row1col5" className="redboard">
+        				</td>
+
+        				<td id="row1col6" className="blackboard">
+        				</td>
+
+        				<td id="row1col7" className="redboard">
+        				</td>
+
+        				<td id="row1col8" className="blackboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row2col1" className="blackboard">
+        				</td>
+
+        				<td id="row2col2" className="redboard">
+        				</td>
+
+        				<td id="row2col3" className="blackboard">
+        				</td>
+
+        				<td id="row2col4" className="redboard">
+        				</td>
+
+        				<td id="row2col5" className="blackboard">
+        				</td>
+
+        				<td id="row2col6" className="redboard">
+        				</td>
+
+        				<td id="row2col7" className="blackboard">
+        				</td>
+
+        				<td id="row2col8" className="redboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row3col1" className="redboard">
+        				</td>
+
+        				<td id="row3col2" className="blackboard">
+        				</td>
+
+        				<td id="row3col3" className="redboard">
+        				</td>
+
+        				<td id="row3col4" className="blackboard"> 
+        				</td>
+
+        				<td id="row3col5" className="redboard">
+        				</td>
+
+        				<td id="row3col6" className="blackboard">
+        				</td>
+
+        				<td id="row3col7" className="redboard">
+        				</td>
+
+        				<td id="row3col8" className="blackboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row4col1" className="blackboard">
+        				</td>
+
+        				<td id="row4col2" className="redboard">
+        				</td>
+
+        				<td id="row4col3" className="blackboard">
+        				</td>
+
+        				<td id="row4col4" className="redboard">
+        				</td>
+
+        				<td id="row4col5" className="blackboard">
+        				</td>
+
+        				<td id="row4col6" className="redboard">
+        				</td>
+
+        				<td id="row4col7" className="blackboard">
+        				</td>
+
+        				<td id="row4col8" className="redboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row5col1" className="redboard">
+        				</td>
+
+        				<td id="row5col2" className="blackboard">
+        				</td>
+
+        				<td id="row5col3" className="redboard">
+        				</td>
+
+        				<td id="row5col4" className="blackboard"> 
+        				</td>
+
+        				<td id="row5col5" className="redboard">
+        				</td>
+
+        				<td id="row5col6" className="blackboard">
+        				</td>
+
+        				<td id="row5col7" className="redboard">
+        				</td>
+
+        				<td id="row5col8" className="blackboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row6col1" className="blackboard">
+        				</td>
+
+        				<td id="row6col2" className="redboard">
+        				</td>
+
+        				<td id="row6col3" className="blackboard">
+        				</td>
+
+        				<td id="row6col4" className="redboard">
+        				</td>
+
+        				<td id="row6col5" className="blackboard">
+        				</td>
+
+        				<td id="row6col6" className="redboard">
+        				</td>
+
+        				<td id="row6col7" className="blackboard">
+        				</td>
+
+        				<td id="row6col8" className="redboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row7col1" className="redboard">
+        				</td>
+
+        				<td id="row7col2" className="blackboard">
+        				</td>
+
+        				<td id="row7col3" className="redboard">
+        				</td>
+
+        				<td id="row7col4" className="blackboard"> 
+        				</td>
+
+        				<td id="row7col5" className="redboard">
+        				</td>
+
+        				<td id="row7col6" className="blackboard">
+        				</td>
+
+        				<td id="row7col7" className="redboard">
+        				</td>
+
+        				<td id="row7col8" className="blackboard">
+        				</td>
+        			</tr>
+
+        			<tr>
+        				<td id="row8col1" className="blackboard">
+        				</td>
+
+        				<td id="row8col2" className="redboard">
+        				</td>
+
+        				<td id="row8col3" className="blackboard">
+        				</td>
+
+        				<td id="row8col4" className="redboard">
+        				</td>
+
+        				<td id="row8col5" className="blackboard">
+        				</td>
+
+        				<td id="row8col6" className="redboard">
+        				</td>
+
+        				<td id="row8col7" className="blackboard">
+        				</td>
+
+        				<td id="row8col8" className="redboard">
+        				</td>
+        			</tr>
+        		</tbody>
+        	</table>
         </div>
       );
     }
