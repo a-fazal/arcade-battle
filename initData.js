4c10b99f3cd2ae76a047bdea80f271db549a636c
Alec Gibson <violinmonkey42@gmail.com>
2019-04-01 22:57:31 -0400

    created user manipulation endpoints for admin, and resetdata endpoint for init

---
+++ initData.js
@@ -0,0 +1,48 @@
+const INIT_USERS = [
+    new User({
+        username: "user",
+        password: "user",
+        role: "user",
+        URI: "",
+        isBanned: false,
+        isPending: false
+    }),
+
+    new User({
+        username: "user2",
+        password: "user2",
+        role: "user",
+        URI: "",
+        isBanned: false,
+        isPending: false
+    }),
+
+    new User({
+        username: "admin",
+        password: "admin",
+        role: "admin",
+        URI: "",
+        isBanned: false,
+        isPending: false
+    }),
+
+    new User({
+        username: "userpending",
+        password: "userpending",
+        role: "user",
+        URI: "",
+        isBanned: false,
+        isPending: true
+    }),
+
+    new User({
+        username: "userbanned",
+        password: "userbanned",
+        role: "user",
+        URI: "",
+        isBanned: true,
+        isPending: false
+    })
+]
+
+export { INIT_USERS }
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 "use strict";
+
+import { INIT_USERS } from './initData'
+
 const log = console.log;
 
 const express = require("express");
@@ -20,10 +23,23 @@ app.use(bodyParser.json());
 //   res.send({ testMessage: 'testing' });
 // });
 
+
+// Delete all model objects
+// add test data to the database
+app.post('/resetdata', (req, res) => {
+  User.deleteMany()
+    .then(function () {
+      INIT_USERS.forEach((u) => {u.save()})
+    })
+  CurrentGame.deleteMany()
+})
+
+/*  USER ENDPOINTS  */
+
 app.get("/allusers", (req, res) => {
   User.find({})
     .select({ password: 0 })
-    .then(function(users) {
+    .then(function (users) {
       if (!users) {
         res.status(404).send();
       } else {
@@ -35,11 +51,67 @@ app.get("/allusers", (req, res) => {
     });
 });
 
+app.post("/acceptuser/:id"), (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+  User.findByIdAndUpdate(
+    id,
+    {isPending: false},
+    (err, user) => {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        return res.send(user);
+      }
+    }
+  )
+}
+
+app.post("/banuser/:id"), (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+  User.findByIdAndUpdate(
+    id,
+    {isBanned: true},
+    (err, user) => {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        return res.send(user);
+      }
+    }
+  )
+}
+
+app.post("/reinstateuser/:id"), (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+  User.findByIdAndUpdate(
+    id,
+    {isBanned: false},
+    (err, user) => {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        return res.send(user);
+      }
+    }
+  )
+}
+
+/*  GAMEPLAY ENDPOINTS  */
+
 app.get("/getopponent/:game", (req, res) => {
   const game = req.params.game
   CurrentGame.findOne({})
     .where('game').equals('tictactoe')
-    .then(function(currentgame) {
+    .then(function (currentgame) {
       if (!currentgame) {
         res.status(404).send();
       } else {
@@ -52,89 +124,87 @@ app.get("/getopponent/:game", (req, res) => {
 });
 
 app.post('/currentgame', (req, res) => {
-
-	const currentgame = new CurrentGame({
-		"startTime":"",
-    "playerOne":req.body.playerOne,
-    "playerTwo":"",
-    "turn":"x",
-    "startOfLastTurn":"",
-    "game":req.body.game,
+  const currentgame = new CurrentGame({
+    "startTime": "",
+    "playerOne": req.body.playerOne,
+    "playerTwo": "",
+    "turn": "x",
+    "startOfLastTurn": "",
+    "game": req.body.game,
     "moves":
-    {"top-left-ttt":"empty","top-center-ttt":"empty","top-right-ttt":"empty","middle-left-ttt":"empty","middle-center-ttt":"empty","middle-right-ttt":"empty","bottom-left-ttt":"empty","bottom-center-ttt":"empty","bottom-right-ttt":"empty"}
-	})
+      { "top-left-ttt": "empty", "top-center-ttt": "empty", "top-right-ttt": "empty", "middle-left-ttt": "empty", "middle-center-ttt": "empty", "middle-right-ttt": "empty", "bottom-left-ttt": "empty", "bottom-center-ttt": "empty", "bottom-right-ttt": "empty" }
+  })
 
-	currentgame.save().then((currentgame) => {
-		res.send(currentgame)
-	}, (error) => {
+  currentgame.save().then((currentgame) => {
+    res.send(currentgame)
+  }, (error) => {
 
-		res.status(400).send(error)
-	})
-
+    res.status(400).send(error)
+  })
 })
 
 app.patch('/currentgame/:id', (req, res) => {
-	const id = req.params.id
-	if (!ObjectID.isValid(id)) {
-		res.status(404).send()
-	}
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
   CurrentGame.findByIdAndUpdate(
-      id,
-      req.body,
-      (err, currentgame) => {
-          if (err) {
-            return res.status(500).send(err);
-          } else {
-              return res.send(currentgame);
-          }
-        }
-    )
+    id,
+    req.body,
+    (err, currentgame) => {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        return res.send(currentgame);
+      }
+    }
+  )
 })
 
 app.get('/currentgamemoves/:id', (req, res) => {
-	const id = req.params.id;
-	if (!ObjectID.isValid(id)) {
-		res.status(404).send()
-	}
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
 
-    CurrentGame.findById(id).then((currentgame) => {
-      if (!currentgame) {
-        res.status(404).send()
-      } else {
-        res.send(currentgame.moves);
-      }
-    }).catch((error) => {
-      res.status(500).send()
-    })
+  CurrentGame.findById(id).then((currentgame) => {
+    if (!currentgame) {
+      res.status(404).send()
+    } else {
+      res.send(currentgame.moves);
+    }
+  }).catch((error) => {
+    res.status(500).send()
+  })
 })
 
 app.patch('/currentgamemoves/:id', (req, res) => {
-	const id = req.params.id
-	if (!ObjectID.isValid(id)) {
-		res.status(404).send()
-	}
-    CurrentGame.findById(id).then((currentgame) => {
-      if (!currentgame) {
-        res.status(404).send()
-      } else {
-        const moves = currentgame.moves
-        moves.set(req.body);
-        currentgame.save().then((currentgame) => {
-          res.send(currentgame)
-        }, (error) => {
-          res.status(400).send(error)
-        })
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+  CurrentGame.findById(id).then((currentgame) => {
+    if (!currentgame) {
+      res.status(404).send()
+    } else {
+      const moves = currentgame.moves
+      moves.set(req.body);
+      currentgame.save().then((currentgame) => {
+        res.send(currentgame)
+      }, (error) => {
+        res.status(400).send(error)
+      })
 
-      }
-    }).catch((error) => {
-      res.status(500).send()
-    })
+    }
+  }).catch((error) => {
+    res.status(500).send()
+  })
 })
 
 // Deploying to Heroku, followed parts from
 // tutorial https://coursework.vschool.io/deploying-mern-with-heroku/
 app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
